9b82ff0703e2a82d58cea3cb3a9061aa9315ee9e
leonardocaballero <leonardocaballero@cfebd454-fee1-11de-a74e-d776d1f25c76>
2010-01-12 17:09:58 +0000

    Agregado nuevas interfaces para los viewlets del logo, footer y colophon

--- trunk/CanaimaAponwao/trunk/canaima.aponwaotheme/canaima/aponwaotheme/browser/interfaces.py
+++ trunk/CanaimaAponwao/trunk/canaima.aponwaotheme/canaima/aponwaotheme/browser/interfaces.py
@@ -1,4 +1,7 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from plone.app.layout.viewlets.interfaces import IPortalFooter
+from zope.viewlet.interfaces import IViewletManager
+#from plone.portlets.interfaces import IPortletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
@@ -6,3 +9,18 @@ class IThemeSpecific(IDefaultPloneLayer):
        "canaima.aponwaotheme" theme, this interface must be its layer
        (in aponwaotheme/viewlets/configure.zcml).
     """
+
+# Viewlet Manager
+class ISiteLogo(IViewletManager):
+    """A viewlet manager to show logo of the site.
+    """
+
+class ISiteFooter(IPortalFooter):
+    """A viewlet manager to show logo of the site.
+    """
+
+class ISiteColophon(IViewletManager):
+    """A viewlet manager to show colophon for anonymous users
+    """
+
+# Portlet Manager
