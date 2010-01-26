b7f2f0481d76668ee1a0f1168f1125b4199ad5a5
leonardocaballero <leonardocaballero@cfebd454-fee1-11de-a74e-d776d1f25c76>
2010-01-26 13:37:54 +0000

    Added config File

---
+++ CanaimaAponwao/trunk/canaima.aponwaopolicy/canaima/aponwaopolicy/config.py
@@ -0,0 +1,6 @@
+
+PRODUCT_DEPENDENCIES = ('RichDocument',
+                        'canaima.aponwaotheme',)
+
+EXTENSION_PROFILES = ('canaima.aponwaopolicy:default',
+                      'canaima.aponwaopolicy:uninstall',)
