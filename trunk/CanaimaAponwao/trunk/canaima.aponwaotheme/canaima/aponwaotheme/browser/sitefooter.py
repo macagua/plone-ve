d4a786a2de32f40f0b2add971a0a347394447c1a
leonardocaballero <leonardocaballero@cfebd454-fee1-11de-a74e-d776d1f25c76>
2010-01-26 15:15:23 +0000

    Clean source code

--- trunk/CanaimaAponwao/trunk/canaima.aponwaotheme/canaima/aponwaotheme/browser/sitefooter.py
+++ trunk/CanaimaAponwao/trunk/canaima.aponwaotheme/canaima/aponwaotheme/browser/sitefooter.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
@@ -5,9 +7,8 @@ class SiteFooterViewlet(ViewletBase):
     render = ViewPageTemplateFile('templates/footer.pt')
     
     def update(self):
-        # seteamos los valores que queremos sacar con el template
-        # ejemplo tonto:
+        ''' definimos los valores que queremos mostrar en el template
+        '''
         self.designer_theme = "Luis Alejandro Martínez Faneyth"
         self.developer_plone_theme = "Leonardo J. Caballero G."
-
 
--- trunk/CanaimaAponwao/trunk/canaima.aponwaotheme/canaima/aponwaotheme/browser/sitelogo.py
+++ trunk/CanaimaAponwao/trunk/canaima.aponwaotheme/canaima/aponwaotheme/browser/sitelogo.py
@@ -1,6 +1,32 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
+#from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets import common
 
-class SiteLogoViewlet(ViewletBase):
-    render = ViewPageTemplateFile('templates/logo.pt')
+#class SiteLogoViewlet(ViewletBase):
+#    render = ViewPageTemplateFile('templates/logo.pt')
 
+class SiteLogoViewlet(common.LogoViewlet):
+    """Alter the logo to include the description of our website
+    """
+
+    def update(self):
+        super(SiteLogoViewlet, self).update()
+        self.navigation_root_url = self.portal_state.navigation_root_url()
+        portal = self.portal_state.portal()
+        logoName = portal.restrictedTraverse('base_properties').logoName
+        self.logo_tag = portal.restrictedTraverse(logoName).tag()
+        self.portal_title = self.portal_state.portal_title()
+
+    def render(self):
+        """
+        By doing this, we are subclassing the LogoViewlet class defined in common.py. Our
+        subclass will automatically do everything the LogoViewlet parent class does, except
+        where we override the parent by telling it to look for a page template known as
+        logo.pt. You may remember that the original LogoViewlet class also used logo.pt
+        ...but there's a big difference this time. Because the new class is inside
+        our theme product, it will find our version of logo.pt, rather than the template in
+        plone.app.layout. 
+        """
+        return self.index()
+    
+    index = ViewPageTemplateFile('templates/logo.pt')
